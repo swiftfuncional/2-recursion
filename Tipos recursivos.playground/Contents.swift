92caa9a42b3a60e7974ff79ea074518ca121e671
Swift Funcional <info@swiftfuncional.com>
2017-06-12 23:00:22 +0200

    Use Swift method defaults, as suggested by @jsmdev

--- Tipos recursivos.playground/Contents.swift
+++ Tipos recursivos.playground/Contents.swift
@@ -11,17 +11,13 @@ let cart = List.NonEmpty(
 					head: 3, tail: List.NonEmpty(
 						head: 4, tail: List.Empty))))))
 
-func totalCost(items: List<Int>) -> Int {
-	func totalCost(items: List<Int>, accumulator: Int) -> Int {
-		switch items {
-		case .Empty:
-			return accumulator
-		case let .NonEmpty(head, tail):
-			return totalCost(items: tail, accumulator: head + accumulator)
-		}
+func totalCost(items: List<Int>, accumulator: Int = 0) -> Int {
+    switch items {
+    case .Empty:
+		return accumulator
+	case let .NonEmpty(head, tail):
+		return totalCost(items: tail, accumulator: head + accumulator)
 	}
-
-	return totalCost(items: cart, accumulator: 0)
 }
 
 
